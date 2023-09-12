b470b52d1cb22223ba45ce53d91babb84d9b710e
Valentin <valkjavalkja@gmail.com>
2023-09-12 11:17:51 -0300

    carousel eventos

--- src/components/Eventos/index.jsx
+++ src/components/Eventos/index.jsx
@@ -1,39 +1,78 @@
 'use client'
-import React, { useEffect, useState } from 'react';
-import styles from './events.module.css'
+import React, { useState } from 'react';
+import styles from './events.module.css';
 import useWindowSize from "@/hooks/useWindowSize";
 
-
 const Eventos = () => {
-
   const windowSize = useWindowSize();
 
+const cards = [
+  {
+    title: 'Evento 1',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    img: './images/pen.png',
+    enlace: 'enlace.comm',
+  },
+  {
+    title: 'Evento 2',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    img: './images/pen.png',
+    enlace: 'enlace.comm',
+  },
+  {
+    title: 'Evento 3',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    img: './images/plants.png',
+    enlace: 'enlace.comm',
+  },
+  {
+    title: 'Evento 4',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    img: './images/pen.png',
+    enlace: 'enlace.comm',
+  },
+  {
+    title: 'Evento 5',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    img: './images/plants.png',
+    enlace: 'enlace.comm',
+  },
+  {
+    title: 'Evento 6',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    img: './images/pen.png',
+    enlace: 'enlace.comm',
+  },
 
+];
 
-    const cards = [
-        {
-          title: 'Evento 1',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-          img: './images/pen.png',
-          enlace: 'enlace.comm',
-        },
-        {
-          title: 'Evento 2',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-          img: './images/pen.png',
-          enlace: 'enlace.comm',
-        },
-        {
-          title: 'Evento 2',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-          img: './images/pen.png',
-          enlace: 'enlace.comm',
-        },
+const [currentCardIndex, setCurrentCardIndex] = useState(0);
+const [animateCards, setAnimateCards] = useState(Array(cards.length).fill(false));
 
-      ];
+const showNextCard = () => {
 
+  const nextIndex = (currentCardIndex + 1) % cards.length;
+  setCurrentCardIndex(nextIndex);
 
-const renderedCards = windowSize.width < 768 ? [cards[0]] : cards; // Muestra solo el primer elemento en pantallas móviles
+  const updatedAnimateCards = Array(cards.length).fill(true);
+  setAnimateCards(updatedAnimateCards);
+
+  setTimeout(() => {
+    const resetAnimateCards = Array(cards.length).fill(false);
+    setAnimateCards(resetAnimateCards);
+  }, 300); 
+};
+
+const visibleCards = [];
+
+if (windowSize.width >= 768) {
+  for (let i = 0; i < 3; i++) {
+    const index = (currentCardIndex + i) % cards.length;
+    visibleCards.push(cards[index]);
+  }
+} else {
+  visibleCards.push(cards[currentCardIndex]);
+}
 
 
   return (
@@ -43,12 +82,22 @@ const renderedCards = windowSize.width < 768 ? [cards[0]] : cards; // Muestra so
           <p>Eventos</p>
         </div>
         <div className={styles.eventsCardContainer}>
-          {renderedCards.map((card, index) => (
-            <div key={index} className={styles.card}>
-              <div className={styles.cardImgContainer}>
+          {visibleCards.map((card, index) => (
+            <div
+              key={index}
+              className={`${styles.card} ${animateCards[index] ? styles.enterAnimation : ''}`}
+            >
+              <div 
+              // className={styles.cardImgContainer}
+              className={`${styles.cardImgContainer} ${animateCards[index] ? styles.cardImgContainerAnimation : ''}`}
+              >
                 <img src={card.img} alt={card.title} />
               </div>
-              <div className={styles.cardContent}>
+              <div 
+              // className={styles.cardContent}
+              className={`${styles.cardContent} ${animateCards[index] ? styles.cardContentAnimation : ''}`}
+              >
+
                 <h2>{card.title}</h2>
                 <p>{card.text}</p>
                 <div className={styles.cardLink}>
@@ -61,9 +110,11 @@ const renderedCards = windowSize.width < 768 ? [cards[0]] : cards; // Muestra so
             </div>
           ))}
         </div>
+        <div className={styles.buttonContainer}>
+          <button onClick={showNextCard}>Siguiente evento</button>
+        </div>
       </div>
     </div>
-
   );
 };
 
